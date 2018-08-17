a3fe671f83beefd6aebe26696c9436c1331b45c3
multi-mod <multimod12@gmail.com>
2018-08-16 23:18:15 -0400

    fixing variable error

--- spamFighter/watcherFunctions/spamCheck.py
+++ spamFighter/watcherFunctions/spamCheck.py
@@ -1,9 +1,9 @@
 def checkForSpam(submissionNumber, lowerPercentage, upperPercentage, submissionPercentage):
 	# return spam if submission number is > 10 with a percentage above upperPercentage
-	if submissionNumber > 10 and percentage >= upperPercentage:
+	if submissionNumber > 10 and submissionPercentage >= upperPercentage:
 		return True
 	# return spam if submission number is > 5 and < 10 wth a percentage above lowerPercentage
-	elif 5 < submissionNumber < 10 and percentage >= lowerPercentage:
+	elif 5 < submissionNumber < 10 and submissionPercentage >= lowerPercentage:
 		return True
 	# return not spam if above conditions for spam not met
 	else:
