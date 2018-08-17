6f156484e97e6d6ea4564fbf7084b9f42d05166c
multi-mod <multimod12@gmail.com>
2018-08-16 23:31:08 -0400

    avoiding type errors

--- spamFighter/watcherFunctions/spamCheck.py
+++ spamFighter/watcherFunctions/spamCheck.py
@@ -1,9 +1,9 @@
 def checkForSpam(submissionNumber, lowerPercentage, upperPercentage, submissionPercentage):
 	# return spam if submission number is > 10 with a percentage above upperPercentage
-	if submissionNumber > 10 and submissionPercentage >= upperPercentage:
+	if int(submissionNumber) > 10 and int(submissionPercentage) >= int(upperPercentage):
 		return True
 	# return spam if submission number is > 5 and < 10 wth a percentage above lowerPercentage
-	elif 5 < submissionNumber < 10 and submissionPercentage >= lowerPercentage:
+	elif 5 < int(submissionNumber) < 10 and int(submissionPercentage) >= int(lowerPercentage):
 		return True
 	# return not spam if above conditions for spam not met
 	else:
